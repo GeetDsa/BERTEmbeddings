6b4d062374293b234369359f8ddceeb69d34f005
Ashwin Geet Dsa <win.12894@gmail.com>
2019-11-04 21:27:58 +0100

    Update sample_usage_bert_embeddings.py

--- sample_usage_bert_embeddings.py
+++ sample_usage_bert_embeddings.py
@@ -3,7 +3,7 @@ print("Note: change BERT_PRETRAINED_DIR= in bertEmbedding.py script")
 
 bertEmbedding = bertEmbeddings.BertEmbeddings() 
 
-embeddings=bertEmbedding.get_features(["killthemall"])
+embeddings=bertEmbedding.get_features(["Hey!! I am the sample usage for bert embeddings."])
 for t,e in embeddings:
     print("token:",t)
     print("embedding:",e)
